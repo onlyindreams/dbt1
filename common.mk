051c718083433e5837c539e4523df8ddd3923702
jztpcw <jztpcw@eccdf8fb-1411-0410-9b6e-c06b97aefaee>
2002-05-21 23:00:02 +0000

    added some macro definitions, removed *_p1.o and *_p2.o
    git-svn-id: https://osdldbt.svn.sourceforge.net/svnroot/osdldbt/trunk/dbt1@11 eccdf8fb-1411-0410-9b6e-c06b97aefaee

--- common.mk
+++ common.mk
@@ -2,69 +2,39 @@
 CC = gcc
 
 SAPDBROOT=/opt/sapdb/interfaces/odbc
-CFLAGS = -g -I../include -I$(SAPDBROOT)/incl
-INCLUDEDIR = ../include
-DBTXNDIR=../interfaces/odbc
-COMMONDIR=../common
-DBDRIVERDIR=../dbdriver
-COMMONOBJS = $(COMMONDIR)/common.o \
-	$(COMMONDIR)/_socket.o
-COMMONPRGS = $(COMMONDIR)/common.c \
-	$(COMMONDIR)/_socket.c
-DBOBJS_P1 = \
-	$(DBTXNDIR)/odbc_interaction_p1.o \
-	$(DBTXNDIR)/odbc_interaction_admin_confirm_p1.o \
-	$(DBTXNDIR)/odbc_interaction_admin_request_p1.o \
-	$(DBTXNDIR)/odbc_interaction_best_sellers_p1.o \
-	$(DBTXNDIR)/odbc_interaction_buy_confirm_p1.o \
-	$(DBTXNDIR)/odbc_interaction_buy_request_p1.o \
-	$(DBTXNDIR)/odbc_interaction_home_p1.o \
-	$(DBTXNDIR)/odbc_interaction_new_products_p1.o \
-	$(DBTXNDIR)/odbc_interaction_order_display_p1.o \
-	$(DBTXNDIR)/odbc_interaction_order_inquiry_p1.o \
-	$(DBTXNDIR)/odbc_interaction_product_detail_p1.o \
-	$(DBTXNDIR)/odbc_interaction_search_request_p1.o \
-	$(DBTXNDIR)/odbc_interaction_search_results_p1.o \
-	$(DBTXNDIR)/odbc_interaction_shopping_cart_p1.o 
-DBOBJS_P2 = $(DBTXNDIR)/odbc_interaction_home_p2.o \
-	$(DBTXNDIR)/odbc_interaction_p2.o \
-	$(DBTXNDIR)/odbc_interaction_shopping_cart_p2.o \
-	$(DBTXNDIR)/odbc_interaction_buy_request_p2.o \
-	$(DBTXNDIR)/odbc_interaction_new_products_p2.o \
-	$(DBTXNDIR)/odbc_interaction_best_sellers_p2.o \
-	$(DBTXNDIR)/odbc_interaction_buy_confirm_p2.o \
-	$(DBTXNDIR)/odbc_interaction_order_inquiry_p2.o \
-	$(DBTXNDIR)/odbc_interaction_order_display_p2.o \
-	$(DBTXNDIR)/odbc_interaction_product_detail_p2.o \
-	$(DBTXNDIR)/odbc_interaction_search_request_p2.o \
-	$(DBTXNDIR)/odbc_interaction_admin_request_p2.o \
-	$(DBTXNDIR)/odbc_interaction_admin_confirm_p2.o \
-	$(DBTXNDIR)/odbc_interaction_search_results_p2.o
-DBPRGS = $(DBTXNDIR)/odbc_interaction_home.c \
-	$(DBTXNDIR)/odbc_interaction.c \
-	$(DBTXNDIR)/odbc_interaction_shopping_cart.c \
-	$(DBTXNDIR)/odbc_interaction_buy_request.c \
-	$(DBTXNDIR)/odbc_interaction_new_products.c \
-	$(DBTXNDIR)/odbc_interaction_best_sellers.c \
-	$(DBTXNDIR)/odbc_interaction_buy_confirm.c \
-	$(DBTXNDIR)/odbc_interaction_order_inquiry.c \
-	$(DBTXNDIR)/odbc_interaction_order_display.c \
-	$(DBTXNDIR)/odbc_interaction_product_detail.c \
-	$(DBTXNDIR)/odbc_interaction_search_request.c \
-	$(DBTXNDIR)/odbc_interaction_admin_request.c \
-	$(DBTXNDIR)/odbc_interaction_admin_confirm.c \
-	$(DBTXNDIR)/odbc_interaction_search_results.c
-DBINCLUDE = $(INCLUDEDIR)/odbc_interaction_home.h \
-	$(INCLUDEDIR)/odbc_interaction.h \
-	$(INCLUDEDIR)/odbc_interaction_shopping_cart.h \
-	$(INCLUDEDIR)/odbc_interaction_buy_request.h \
-	$(INCLUDEDIR)/odbc_interaction_new_products.h \
-	$(INCLUDEDIR)/odbc_interaction_best_sellers.h \
-	$(INCLUDEDIR)/odbc_interaction_buy_confirm.h \
-	$(INCLUDEDIR)/odbc_interaction_order_inquiry.h \
-	$(INCLUDEDIR)/odbc_interaction_order_display.h \
-	$(INCLUDEDIR)/odbc_interaction_product_detail.h \
-	$(INCLUDEDIR)/odbc_interaction_search_request.h \
-	$(INCLUDEDIR)/odbc_interaction_admin_request.h \
-	$(INCLUDEDIR)/odbc_interaction_admin_confirm.h \
-	$(INCLUDEDIR)/odbc_interaction_search_results.h
+CFLAGS = -g -I$(INCLUDE_DIR) -I$(SAPDBROOT)/incl -DDEBUG -DGET_TIME
+
+COMMON_PRGS = $(COMMON_DIR)/common.c $(COMMON_DIR)/_socket.c
+COMMON_OBJS = $(COMMON_PRGS:.c=.o)
+
+ODBC_PRGS = \
+	$(ODBC_DIR)/odbc_interaction.c \
+	$(ODBC_DIR)/odbc_interaction_admin_confirm.c \
+	$(ODBC_DIR)/odbc_interaction_admin_request.c \
+	$(ODBC_DIR)/odbc_interaction_best_sellers.c \
+	$(ODBC_DIR)/odbc_interaction_buy_confirm.c \
+	$(ODBC_DIR)/odbc_interaction_buy_request.c \
+	$(ODBC_DIR)/odbc_interaction_home.c \
+	$(ODBC_DIR)/odbc_interaction_new_products.c \
+	$(ODBC_DIR)/odbc_interaction_order_display.c \
+	$(ODBC_DIR)/odbc_interaction_order_inquiry.c \
+	$(ODBC_DIR)/odbc_interaction_product_detail.c \
+	$(ODBC_DIR)/odbc_interaction_search_request.c \
+	$(ODBC_DIR)/odbc_interaction_search_results.c \
+	$(ODBC_DIR)/odbc_interaction_shopping_cart.c 
+ODBC_OBJS = $(ODBC_PRGS:.c=.o)
+
+ODBC_INCLUDE = $(INCLUDE_DIR)/odbc_interaction_home.h \
+	$(INCLUDE_DIR)/odbc_interaction.h \
+	$(INCLUDE_DIR)/odbc_interaction_shopping_cart.h \
+	$(INCLUDE_DIR)/odbc_interaction_buy_request.h \
+	$(INCLUDE_DIR)/odbc_interaction_new_products.h \
+	$(INCLUDE_DIR)/odbc_interaction_best_sellers.h \
+	$(INCLUDE_DIR)/odbc_interaction_buy_confirm.h \
+	$(INCLUDE_DIR)/odbc_interaction_order_inquiry.h \
+	$(INCLUDE_DIR)/odbc_interaction_order_display.h \
+	$(INCLUDE_DIR)/odbc_interaction_product_detail.h \
+	$(INCLUDE_DIR)/odbc_interaction_search_request.h \
+	$(INCLUDE_DIR)/odbc_interaction_admin_request.h \
+	$(INCLUDE_DIR)/odbc_interaction_admin_confirm.h \
+	$(INCLUDE_DIR)/odbc_interaction_search_results.h
