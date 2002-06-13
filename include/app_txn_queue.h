4dc5a154c485bae41256b30f48dddd4b7db7c9d9
jztpcw <jztpcw@eccdf8fb-1411-0410-9b6e-c06b97aefaee>
2002-06-13 22:01:56 +0000

    added state to QItem structure. The state is used to prevent reusing queue slot when database response time is long
    git-svn-id: https://osdldbt.svn.sourceforge.net/svnroot/osdldbt/trunk/dbt1@29 eccdf8fb-1411-0410-9b6e-c06b97aefaee

--- include/app_txn_queue.h
+++ include/app_txn_queue.h
@@ -12,20 +12,35 @@
 #define _APP_TXN_QUEUE_H
 #include <pthread.h>
 
+/* three states of the QItem: 0 - empty 1 - full, waiting to be dequeued 
+ * 2 - being dequeued, but still waiting for the transaction to return
+ * from database. This state is used to prevent reusing the slot when
+ * database response time is long
+ */
+#define EMPTY 0
+#define FULL 1
+#define WAIT 2
+
 struct QItem
 {
 	int TxnType;
 	int SlotID;
+	int state;
 };
 
 struct Queue
 {
 	int front;
 	int rear;
+	/* items is the number of items in the queue that 
+         * have not been dequeued*/
 	int items;
 	int size;
+	/* capacity is the number of slots in the queue that is EMPTY*/
+	int capacity;
 	struct QItem *TxnQ;
 };
+
 
 void init_queue(struct Queue *txn_queue, int TxnQSize);
 int isempty(struct Queue txn_queue);
@@ -33,5 +48,6 @@ int isfull(struct Queue txn_queue);
 int queuecount(struct Queue txn_queue);
 int enqueue(struct QItem Item, struct Queue *txn_queue);
 int dequeue(struct QItem *Item, struct Queue *txn_queue);
+void empty_queue_item_state(struct Queue *txn_queue, int QIndex);
 
 #endif
