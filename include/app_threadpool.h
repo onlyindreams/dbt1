7cf0717146d05aca7c7018b1fc757b45b613cf74
jztpcw <jztpcw@eccdf8fb-1411-0410-9b6e-c06b97aefaee>
2002-06-13 21:58:32 +0000

    added condition_bundle_t structure definition. This structure is used to signal DoConnection thread when transaction is done
    git-svn-id: https://osdldbt.svn.sourceforge.net/svnroot/osdldbt/trunk/dbt1@28 eccdf8fb-1411-0410-9b6e-c06b97aefaee

--- include/app_threadpool.h
+++ include/app_threadpool.h
@@ -11,9 +11,17 @@
 #ifndef _APP_THREADPOOL_H
 #define _APP_THREADPOOL_H_
 #include "odbc_interaction.h"
+#include "app_txn_queue.h"
+
+struct condition_bundle_t {
+	int txn_done_flag;
+	pthread_mutex_t condition_mutex;
+	pthread_cond_t txn_done_cv;
+};
 
 int init_thread_pool(int PoolThreads, int TxnQSize, char *sname, char *uname, char *auth);
 void *DoTxn(void *fd);
+void set_txn_done_flag(struct Queue *txn_queue, int QIndex);
 #ifdef _SIMDB
 void fill_promo_data(struct promotional_processing_t *pp_data);
 #endif
