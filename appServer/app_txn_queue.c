396667ef873e6deb46eb479d7d6dba1495890318
jztpcw <jztpcw@eccdf8fb-1411-0410-9b6e-c06b97aefaee>
2002-06-13 22:09:40 +0000

    added state to QItem structure to prevent reusing queue item when database response time is long. Changed isfull, enqueue, and dequeue functions accordingly
    git-svn-id: https://osdldbt.svn.sourceforge.net/svnroot/osdldbt/trunk/dbt1@32 eccdf8fb-1411-0410-9b6e-c06b97aefaee

--- appServer/app_txn_queue.c
+++ appServer/app_txn_queue.c
@@ -16,11 +16,15 @@ extern pthread_mutex_t *queue_entry_mutex;
 
 void init_queue(struct Queue *txn_queue, int TxnQSize)
 {
+	int i;
 	txn_queue->front=0;
 	txn_queue->rear=0;
 	txn_queue->items=0;
 	txn_queue->size=TxnQSize;
+	txn_queue->capacity=TxnQSize;
 	txn_queue->TxnQ=(struct QItem *)malloc(sizeof(struct QItem)*TxnQSize);
+	for (i=0; i<TxnQSize; i++)
+		txn_queue->TxnQ[i].state=EMPTY;	
 }
 
 int isempty(struct Queue txn_queue)
@@ -31,10 +35,11 @@ int isempty(struct Queue txn_queue)
 
 int isfull(struct Queue txn_queue)
 {
-	if (txn_queue.items==txn_queue.size) return TRUE;
+	if (txn_queue.capacity==0) return TRUE;
 	return FALSE;
 }
 
+/* return the number of items in the queue that have not been dequeued */
 int queuecount(struct Queue txn_queue)
 {
 	return txn_queue.items;
@@ -43,17 +48,29 @@ int queuecount(struct Queue txn_queue)
 int enqueue(struct QItem Item, struct Queue *txn_queue)
 {
 	if (isfull(*txn_queue)) return -1;
+	/* find one slot at FRONT that is EMPTY */
 	if (!isempty(*txn_queue))
 	{
-	   txn_queue->rear++;
-	   if (txn_queue->rear==txn_queue->size) txn_queue->rear=0;
+		do {	
+			txn_queue->rear++;
+			if (txn_queue->rear==txn_queue->size) txn_queue->rear=0;
+		} while (txn_queue->TxnQ[txn_queue->rear].state!=EMPTY);
 	}
+	else
+	{
+		while (txn_queue->TxnQ[txn_queue->rear].state!=EMPTY) 
+		{
+			txn_queue->rear++;
+			if (txn_queue->rear==txn_queue->size) txn_queue->rear=0;
+		}
+	}
+			
+	txn_queue->TxnQ[txn_queue->rear].state=FULL;
 	txn_queue->TxnQ[txn_queue->rear].TxnType=Item.TxnType;
 	txn_queue->TxnQ[txn_queue->rear].SlotID=Item.SlotID;
-	// --TxnQ[rear].pid=Item.pid;
 	txn_queue->items++;
-	// lock so that DoConnect will wait
-	pthread_mutex_lock(&queue_entry_mutex[txn_queue->rear]);
+	txn_queue->capacity--;
+	
 	return txn_queue->rear;
 }
 
@@ -61,13 +78,25 @@ int dequeue(struct QItem *Item, struct Queue *txn_queue)
 {
 	int cur_pos;
 	if (isempty(*txn_queue)) return -1;
+	while (txn_queue->TxnQ[txn_queue->front].state!=FULL) 
+	{
+		txn_queue->front++;
+		if (txn_queue->front == txn_queue->size) txn_queue->front=0;
+	}
 	cur_pos = txn_queue->front;
 	Item->TxnType = txn_queue->TxnQ[txn_queue->front].TxnType;
 	Item->SlotID = txn_queue->TxnQ[txn_queue->front].SlotID;
+	txn_queue->TxnQ[txn_queue->front].state=WAIT;
 	txn_queue->items--;
 	txn_queue->front++;
 	if (txn_queue->front == txn_queue->size) txn_queue->front=0;
 	if (txn_queue->items == 0) txn_queue->rear=txn_queue->front;
  
 	return cur_pos;
+}
+
+void empty_queue_item_state(struct Queue *txn_queue, int QIndex)
+{
+	txn_queue->capacity++;
+	txn_queue->TxnQ[QIndex].state=EMPTY;
 }
