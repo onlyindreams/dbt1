5dc72f8136405222883dace83c8d41410e995947
jztpcw <jztpcw@eccdf8fb-1411-0410-9b6e-c06b97aefaee>
2002-06-27 00:03:40 +0000

    socket interface
    git-svn-id: https://osdldbt.svn.sourceforge.net/svnroot/osdldbt/trunk/dbt1@54 eccdf8fb-1411-0410-9b6e-c06b97aefaee

---
+++ cache/cache_interface.c
@@ -0,0 +1,49 @@
+/* tm_interface.c
+ *
+ * This file is released under the terms of the Artistic License.  Please see
+ * the file LICENSE, included in this package, for details.
+ *
+ * Copyright (C) 2002 Mark Wong & Jenny Zhang &
+ *                    Open Source Development Lab, Inc.
+ *
+ * 18 march 2002
+ */
+
+#include "cache_interface.h"
+
+int receive_search_results_request(int s, struct search_results_t *search_results_request)
+{
+	void *data;
+	int length;
+	int rec;
+
+	/* Receive search_results request data. */
+	length=sizeof(struct search_results_t);
+
+	if ((rec=_receive(s, search_results_request, length)) == -1)
+	{
+		LOG_ERROR_MESSAGE("cannot receive search results request");
+		return W_ERROR;
+	}
+	if (rec==0)
+	{
+		return SOCKET_CLOSE;
+	}
+
+	return W_OK;
+}
+
+int send_search_results_response(int s,  struct search_results_t *search_results_response)
+{
+	int length;
+
+	length=sizeof(struct search_results_t);
+	if (_send(s, search_results_response, length) == -1)
+	{
+		LOG_ERROR_MESSAGE("cannot send search_results, errno %d", errno);
+		return W_ERROR;
+	}
+
+	return W_OK;
+}
+
