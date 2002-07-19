eeeea41032862d5f317a8e87193f31054f5fd1c1
jztpcw <jztpcw@eccdf8fb-1411-0410-9b6e-c06b97aefaee>
2002-07-19 16:25:50 +0000

    *** empty log message ***
    git-svn-id: https://osdldbt.svn.sourceforge.net/svnroot/osdldbt/trunk/dbt1@84 eccdf8fb-1411-0410-9b6e-c06b97aefaee

--- scripts/stats/cpu.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Collect the first 60 minutes of data of CPU data, at 30 second intervals.
-iostat -c $1 $2 > cpu.txt
-
-# Reformat the data into a comma delmited format after the run to minimize the
-# CPU overhead.
-cat cpu.txt | grep "\." | awk '{ print $1","$2","$3","$4 }' > cpu.csv
