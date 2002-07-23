701f406265c0aa5b623df4406f176430fc944000
jztpcw <jztpcw@eccdf8fb-1411-0410-9b6e-c06b97aefaee>
2002-07-23 21:12:21 +0000

    use c_discount as input instead of selecting from shopping_cart
    git-svn-id: https://osdldbt.svn.sourceforge.net/svnroot/osdldbt/trunk/dbt1@104 eccdf8fb-1411-0410-9b6e-c06b97aefaee

--- storedproc/sapdb/updateSC.sql
+++ storedproc/sapdb/updateSC.sql
@@ -8,13 +8,14 @@
 /*
 /*/
 CREATE DBPROC updateSC (IN sc_id fixed(10,0), 
-OUT  sc_sub_total fixed(17,2), OUT discount fixed(5,2), 
+IN discount fixed(5,2), OUT  sc_sub_total fixed(17,2), 
 OUT sc_tax fixed(17,2), OUT sc_ship_cost fixed(5,2), 
 OUT sc_total fixed(17,2)) AS
 VAR i_cost fixed(17,2); i_id fixed(10,0); sub_total fixed(17,2); 
     scl_qty fixed(3, 0); 
 SUBTRANS BEGIN;
 set sc_sub_total=0.00;
+set discount=0.00;
 set sc_tax=0.00;
 set sc_ship_cost=0.00;
 set sc_total=0.00;
@@ -28,11 +29,10 @@ WHILE $rc=0 DO BEGIN
   END;
 END;
 CALL getSCSubTotal(:sc_id, :sub_total);
-SELECT sc_c_discount INTO :discount FROM dbt.shopping_cart WHERE sc_id=:sc_id;
-SELECT sum(scl_qty) INTO :scl_qty 
+SELECT sum(scl_qty) INTO :scl_qty
   FROM dbt.shopping_cart_line WHERE scl_sc_id=:sc_id;
 set sc_sub_total=sub_total*(1-discount);
-set sc_tax=sc_sub_total*0.0825; 
+set sc_tax=sc_sub_total*0.0825;
 set sc_ship_cost=3.00+(1.00*scl_qty);
 set sc_total=sc_sub_total+sc_ship_cost+sc_tax;
 UPDATE dbt.shopping_cart set sc_date=(timestamp), sc_sub_total=(:sc_sub_total),
