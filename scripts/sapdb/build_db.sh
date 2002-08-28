e0c10979f68522636b48441815ba8b055bf3cf6a
jztpcw <jztpcw@eccdf8fb-1411-0410-9b6e-c06b97aefaee>
2002-08-28 20:29:12 +0000

    now accept command line argument '-g' to generate datafile before loading the database
    git-svn-id: https://osdldbt.svn.sourceforge.net/svnroot/osdldbt/trunk/dbt1@247 eccdf8fb-1411-0410-9b6e-c06b97aefaee

--- scripts/sapdb/build_db.sh
+++ scripts/sapdb/build_db.sh
@@ -1,54 +1,72 @@
 #!/bin/sh
-#this script can be used to build the database
+#this script is used to build the database
+# ./build_db.sh -g <items> <eus> will generate data and build the database
+# ./build_db.sh will load the database from previously generated data files
 
-date
-
-echo "drop db"
-./drop_db.sh
-echo
-
-echo "create db"
-./create_db.sh
-echo
-
-echo "create tables"
-./create_tables.sh
-echo
-
-echo "start to load db"
-date
-
-echo "load db"
-./load_db.sh
-echo
-
-echo "start to create index"
-date
-
-echo "create indexes"
-./create_indexes.sh
-echo
-
-echo "start to create keys"
-date
-
-echo "create keys"
-./create_keys.sh
-echo
-
-echo "create sequences"
-./create_sequences.sh
-echo
-
-echo "load dbproc"
-./load_dbproc.sh
-echo
-
-echo "start to backup"
-date
-
-echo "backup"
-./backup_db.sh
-echo
-
-date
+if [ $# -ne 0 ] # Script invoked with command-line args
+then
+	getopts "g" Option
+	if [ $Option != "g" ]
+		then echo "usage: $0 -g <items> <eus>" 
+	else
+		if [ $# -ne 3 ]
+			then echo "usage: $0 -g <items> <eus>" 
+		else
+			ITEMS=$2
+			EUS=$3	
+		fi
+	fi
+	echo "Generating data... $ITEMS item and $EUS eu"
+	cd ../../datagen
+	date
+	./datagen $ITEMS $EUS
+	echo "data files are generated"
+	date
+else
+	echo "build the database without generating the data files"
+	echo "drop db"
+	./drop_db.sh
+	echo
+	
+	echo "create db"
+	./create_db.sh
+	echo
+	
+	echo "create tables"
+	./create_tables.sh
+	echo
+	
+	echo "start to load db"
+	date
+	
+	echo "load db"
+	./load_db.sh
+	echo
+	
+	echo "starting to create indexes"
+	date
+	
+	echo "creating indexes"
+	./create_indexes.sh
+	echo
+	
+	echo "starting to create keys"
+	date
+	
+	echo "creating keys"
+	./create_keys.sh
+	echo
+	
+	echo "loading stored procedures"
+	./load_dbproc.sh
+	echo
+	
+	echo "starting to backup database"
+	date
+	
+	echo "backup"
+	./backup_db.sh
+	echo
+	
+	date
+fi
