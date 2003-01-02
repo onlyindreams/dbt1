04a2ac713ed1b9ad269079dddccdd83cb3dd7b52
jztpcw <jztpcw@eccdf8fb-1411-0410-9b6e-c06b97aefaee>
2003-01-02 18:22:36 +0000

    moved medium definition from create_db.sh to backup_db.sh
    git-svn-id: https://osdldbt.svn.sourceforge.net/svnroot/osdldbt/trunk/dbt1@537 eccdf8fb-1411-0410-9b6e-c06b97aefaee

--- scripts/sapdb/backup_db.sh
+++ scripts/sapdb/backup_db.sh
@@ -21,6 +21,9 @@ echo "start backup"
 _o=`cat <<EOF | /opt/sapdb/depend/bin/dbmcli -d DBT1 -u dbm,dbm 2>&1
 db_stop
 db_start
+medium_put data /dbt1/datasave FILE DATA 0 8 YES
+medium_put incr /dbt1/incremental FILE PAGES 0 8 YES
+medium_put auto /dbt1/autosave FILE AUTO
 util_connect dbm,dbm
 backup_start data migration
 backup_start incr migration
@@ -33,22 +36,7 @@ if [ "$_test" = "" ]; then
         echo "backup failed: $_o"
         exit 1
 fi
-date
+echo "backup done"
 
-#echo "changing data_cache back to 262144"
-#_o=`cat <<EOF |  /opt/sapdb/depend/bin/dbmcli -d $SID -u dbm,dbm 2>&1
-#param_startsession
-#param_put DATA_CACHE 262144
-#param_put _IDXFILE_LIST_SIZE 8192
-#param_checkall
-#param_commitsession
-#db_stop
-#db_start
-#db_warm
-#quit
-#EOF`
-#_test=`echo $_o | grep ERR`
-#if ! [ "$_test" = "" ]; then
-#        echo "set parameters failed"
-#        exit 1
-#fi
+#echo "set database parameters"
+./set_param.sh
