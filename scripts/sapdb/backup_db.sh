6913353014ccb920d346cb5f6c4c1d33dc78c03b
jztpcw <jztpcw@eccdf8fb-1411-0410-9b6e-c06b97aefaee>
2003-01-15 22:55:53 +0000

    call define_medium.sh and set_param.sh
    git-svn-id: https://osdldbt.svn.sourceforge.net/svnroot/osdldbt/trunk/dbt1@620 eccdf8fb-1411-0410-9b6e-c06b97aefaee

--- scripts/sapdb/backup_db.sh
+++ scripts/sapdb/backup_db.sh
@@ -1,34 +1,29 @@
 #!/bin/sh
 
-#note: if DATA_CACHE is too big, it should be reduced before backup
-#otherwise we get I/O error
-#echo "changing data_cache to 10000"
-#_o=`cat <<EOF |  /opt/sapdb/depend/bin/dbmcli -d $SID -u dbm,dbm 2>&1
-#param_startsession
-#param_put DATA_CACHE 10000
-#param_checkall
-#param_commitsession
-#quit
-#EOF`
-#_test=`echo $_o | grep ERR`
-#if ! [ "$_test" = "" ]; then
-#        echo "set parameters failed"
-#        exit 1
-#fi
+echo "changing data_cache to 10000"
+_o=`cat <<EOF |  /opt/sapdb/depend/bin/dbmcli -d $SID1 -u dbm,dbm 2>&1
+param_startsession
+param_put DATA_CACHE 10000
+param_checkall
+param_commitsession
+quit
+EOF`
+_test=`echo $_o | grep ERR`
+if ! [ "$_test" = "" ]; then
+        echo "set parameters failed"
+        exit 1
+fi
 
 date
 echo "start backup"
-_o=`cat <<EOF | /opt/sapdb/depend/bin/dbmcli -d DBT1 -u dbm,dbm 2>&1
+./define_medium.sh
+_o=`cat <<EOF | /opt/sapdb/depend/bin/dbmcli -d $SID1 -u dbm,dbm 2>&1
 db_stop
 db_start
-medium_put data /dbt1/datasave FILE DATA 0 8 YES
-medium_put incr /dbt1/incremental FILE PAGES 0 8 YES
-medium_put auto /dbt1/autosave FILE AUTO
 util_connect dbm,dbm
 backup_start data migration
 backup_start incr migration
 util_release
-db_warm
 quit
 EOF`
 _test=`echo $_o | grep OK`
@@ -36,7 +31,12 @@ if [ "$_test" = "" ]; then
         echo "backup failed: $_o"
         exit 1
 fi
-echo "backup done"
+date
 
-#echo "set database parameters"
-./set_param.sh
+echo "changing data_cache back to 262144"
+./set_param.sh 1
+_o=`cat <<EOF | /opt/sapdb/depend/bin/dbmcli -d $SID1 -u dbm,dbm 2>&1
+db_stop
+db_warm
+quit
+EOF`
