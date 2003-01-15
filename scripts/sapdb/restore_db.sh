87678e7d6022bb136d5574fcf803a131967a3e55
jztpcw <jztpcw@eccdf8fb-1411-0410-9b6e-c06b97aefaee>
2003-01-15 23:33:57 +0000

    added calling define_mediium and set_param
    git-svn-id: https://osdldbt.svn.sourceforge.net/svnroot/osdldbt/trunk/dbt1@631 eccdf8fb-1411-0410-9b6e-c06b97aefaee

--- scripts/sapdb/restore_db.sh
+++ scripts/sapdb/restore_db.sh
@@ -1,31 +1,29 @@
 #!/bin/sh
 
-#note: if DATA_CACHE is too big, it should be reduced before backup
-#otherwise we get I/O error
-#echo "changing data_cache to 10000"
-#_o=`cat <<EOF |  /opt/sapdb/depend/bin/dbmcli -d $SID1 -u dbm,dbm 2>&1
-#param_startsession
-#param_put DATA_CACHE 10000
-#param_checkall
-#param_commitsession
-#quit
-#EOF`
-#_test=`echo $_o | grep ERR`
-#if ! [ "$_test" = "" ]; then
-#        echo "set parameters failed"
-#        exit 1
-#fi
+echo "changing data_cache to 10000"
+_o=`cat <<EOF |  /opt/sapdb/depend/bin/dbmcli -d $SID1 -u dbm,dbm 2>&1
+param_startsession
+param_put DATA_CACHE 10000
+param_checkall
+param_commitsession
+quit
+EOF`
+_test=`echo $_o | grep ERR`
+if ! [ "$_test" = "" ]; then
+        echo "set parameters failed"
+        exit 1
+fi
 
 echo "restore db"
+./define_medium.sh
 _o=`cat <<EOF | dbmcli -d $SID1 -u dbm,dbm 2>&1
 db_stop
-db_cold
+db_start
 util_connect dbm,dbm
 util_execute INIT CONFIG
 recover_start data
 recover_start incr
 util_release
-db_warm
 quit
 EOF`
 _test=`echo $_o | grep OK`
@@ -34,20 +32,15 @@ if [ "$_test" = "" ]; then
         exit 1
 fi
 
-#echo "changing data_cache back to 262144"
-#_o=`cat <<EOF |  /opt/sapdb/depend/bin/dbmcli -d $SID1 -u dbm,dbm 2>&1
-#param_startsession
-#param_put DATA_CACHE 262144
-#param_put _IDXFILE_LIST_SIZE 8192
-#param_checkall
-#param_commitsession
-#db_stop
-#db_start
-#db_warm
-#quit
-#EOF`
-#_test=`echo $_o | grep ERR`
-#if ! [ "$_test" = "" ]; then
-#        echo "set parameters failed"
-#        exit 1
-#fi
+echo "changing data_cache back to 262144"
+./set_param.sh 1
+_o=`cat <<EOF | /opt/sapdb/depend/bin/dbmcli -d $SID1 -u dbm,dbm 2>&1
+db_stop
+db_warm
+quit
+EOF`
+_test=`echo $_o | grep ERR`
+if ! [ "$_test" = "" ]; then
+        echo "set parameters failed"
+        exit 1
+fi
