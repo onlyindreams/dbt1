9d328a0b206460b823223714c82350e81bb20579
jztpcw <jztpcw@eccdf8fb-1411-0410-9b6e-c06b97aefaee>
2003-01-16 22:34:12 +0000

    add bouncing database at the end if it is called using parameter 1
    git-svn-id: https://osdldbt.svn.sourceforge.net/svnroot/osdldbt/trunk/dbt1@638 eccdf8fb-1411-0410-9b6e-c06b97aefaee

--- scripts/sapdb/set_param.sh
+++ scripts/sapdb/set_param.sh
@@ -1,22 +1,50 @@
 #!/bin/sh
 
+if [ $# -ne 1 ]; then
+	echo "Usage: ./set_param.sh <0/1>"
+	exit
+fi
+
 # setup database parameters
 echo "set parameters for $SID1..."
-_o=`cat <<EOF | dbmcli -d $SID1 -u dbm,dbm 2>&1
-param_rmfile
-param_startsession
-param_init
-param_put LOG_MODE DEMO
-param_put DATA_CACHE 5000
-param_put MAXUSERTASKS 10
-param_put MAXCPU 2
-param_put _IDXFILE_LIST_SIZE 8192
-param_put _PACKET_SIZE 131072
-param_put DATE_TIME_FORMAT ISO
-param_checkall
-param_commitsession
-quit
-EOF`
+if [ $1 -eq 0 ]; then
+	_o=`cat <<EOF | dbmcli -d $SID1 -u dbm,dbm 2>&1
+	param_rmfile
+	param_startsession
+	param_init
+	param_put LOG_MODE DEMO
+	param_put DATA_CACHE 262144
+	param_put MAXDATADEVSPACES 14
+	param_put MAXUSERTASKS 100
+	param_put MAXCPU 8
+	param_put _IDXFILE_LIST_SIZE 8192
+	param_put _PACKET_SIZE 131072
+	param_put CAT_CACHE_SUPPLY 6464
+	param_put _USE_IOPROCS_ONLY YES
+	param_checkall
+	param_commitsession
+	quit
+	EOF`
+fi
+if [ $1 -eq 1 ]; then
+	_o=`cat <<EOF | dbmcli -d $SID1 -u dbm,dbm 2>&1
+	param_startsession
+	param_put LOG_MODE DEMO
+	param_put DATA_CACHE 262144
+	param_put MAXDATADEVSPACES 14
+	param_put MAXUSERTASKS 100
+	param_put MAXCPU 8
+	param_put _IDXFILE_LIST_SIZE 8192
+	param_put _PACKET_SIZE 131072
+	param_put CAT_CACHE_SUPPLY 6464
+	param_put _USE_IOPROCS_ONLY NO
+	param_checkall
+	param_commitsession
+	db_stop
+	db_warm
+	quit
+	EOF`
+fi
 _test=`echo $_o | grep OK`
 if [ "$_test" = "" ]; then
         echo "set parameters failed: $_o"
