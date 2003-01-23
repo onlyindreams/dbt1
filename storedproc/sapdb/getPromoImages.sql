e640fdd92c3db705b4c8dd453ba2e1edd827792d
fimath <fimath@eccdf8fb-1411-0410-9b6e-c06b97aefaee>
2003-01-23 23:34:26 +0000

    Was told using WHERE..IN doesn't guarantee correctness when we're trying to query pairs.  Using a slow, but correct method for now.
    git-svn-id: https://osdldbt.svn.sourceforge.net/svnroot/osdldbt/trunk/dbt1@654 eccdf8fb-1411-0410-9b6e-c06b97aefaee

--- storedproc/sapdb/getPromoImages.sql
+++ storedproc/sapdb/getPromoImages.sql
@@ -7,19 +7,36 @@
 /*                    Open Source Development Lab, Inc.
 /*
 /*/
-CREATE DBPROC GetPromoImages(IN I_ID fixed(10, 0), 
+CREATE DBPROC GetPromoImages(IN I_ID fixed(10), 
 OUT I_ID1 fixed(10), OUT I_T1 fixed(10), 
 OUT I_ID2 fixed(10), OUT I_T2 fixed(10), 
 OUT I_ID3 fixed(10), OUT I_T3 fixed(10), 
 OUT I_ID4 fixed(10), OUT I_T4 fixed(10), 
 OUT I_ID5 fixed(10), OUT I_T5 fixed(10)) AS 
-        VAR r1 fixed(10); r2 fixed(10);r3 fixed(10);r4 fixed(10);r5 fixed(10);
+  VAR r1 fixed(10); r2 fixed(10); r3 fixed(10); r4 fixed(10); r5 fixed(10);
 BEGIN
-SELECT i_related1, i_related2, i_related3, i_related4, i_related5 INTO :r1, :r2, :r3, :r4, :r5 FROM dbt.item WHERE i_id = :I_ID;
-SELECT i_id, i_thumbnail FROM dbt.item WHERE i_id IN (:r1, :r2, :r3, :r4, :r5); 
-FETCH INTO :I_ID1, :I_T1;
-FETCH INTO :I_ID2, :I_T2;
-FETCH INTO :I_ID3, :I_T3;
-FETCH INTO :I_ID4, :I_T4;
-FETCH INTO :I_ID5, :I_T5;
+  SELECT i_related1, i_related2, i_related3, i_related4, i_related5
+  INTO :r1, :r2, :r3, :r4, :r5
+  FROM dbt.item
+  WHERE i_id = :I_ID;
+  SELECT i_id, i_thumbnail
+  INTO :I_ID1, :I_T1
+  FROM dbt.item
+  WHERE i_id = :r1;
+  SELECT i_id, i_thumbnail
+  INTO :I_ID2, :I_T2
+  FROM dbt.item
+  WHERE i_id = :r2;
+  SELECT i_id, i_thumbnail
+  INTO :I_ID3, :I_T3
+  FROM dbt.item
+  WHERE i_id = :r3;
+  SELECT i_id, i_thumbnail
+  INTO :I_ID4, :I_T4
+  FROM dbt.item
+  WHERE i_id = :r4;
+  SELECT i_id, i_thumbnail
+  INTO :I_ID5, :I_T5
+  FROM dbt.item
+  WHERE i_id = :r5;
 END;;
