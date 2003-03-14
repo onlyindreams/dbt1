8d679210d73dc909c671fd89814fcf3bded787f3
jztpcw <jztpcw@eccdf8fb-1411-0410-9b6e-c06b97aefaee>
2003-03-14 19:47:28 +0000

    format clean up
    git-svn-id: https://osdldbt.svn.sourceforge.net/svnroot/osdldbt/trunk/dbt1@819 eccdf8fb-1411-0410-9b6e-c06b97aefaee

--- appServer/app_interface.c
+++ appServer/app_interface.c
@@ -1,4 +1,5 @@
-/* tm_interface.c
+/*
+ * app_interface.c
  *
  * This file is released under the terms of the Artistic License.  Please see
  * the file LICENSE, included in this package, for details.
@@ -39,149 +40,149 @@ int receive_transaction_packet(int s, struct QItem *TxnQItem)
 	int rec;
 
 	/* Receive transaction type. */
-	if ( (rec=_receive(s, (void *) &interaction, sizeof(interaction))) == -1)
+	if ((rec = _receive(s, (void *) &interaction, sizeof(interaction))) == -1)
 	{
 		LOG_ERROR_MESSAGE("cannot receive interaction type");
 		return W_ERROR;
 	}
-	if (rec==SOCKET_CLOSE) return SOCKET_CLOSE;
+	if (rec == SOCKET_CLOSE) return SOCKET_CLOSE;
 
-	TxnQItem->TxnType=interaction;
+	TxnQItem->TxnType = interaction;
 	/* Receive transaction data. */
 	switch (interaction)
 	{
 		case ADMIN_CONFIRM:
-			interaction_q_slot_id=PinSlot(&app_admin_confirm_array);
-			if (interaction_q_slot_id==-1)
+			interaction_q_slot_id = PinSlot(&app_admin_confirm_array);
+			if (interaction_q_slot_id == -1)
 			{
 				LOG_ERROR_MESSAGE("PinSlot failed");
 				return W_ERROR;
 			}
-			data = &(app_admin_confirm_array.odbc_data_array[interaction_q_slot_id].admin_confirm_odbc_data.eb);
+			data = &(app_admin_confirm_array.data_array[interaction_q_slot_id].admin_confirm_data);
 			length = sizeof(struct admin_confirm_t);
 			break;
 		case ADMIN_REQUEST:
-			interaction_q_slot_id=PinSlot(&app_admin_request_array);
-			if (interaction_q_slot_id==-1)
+			interaction_q_slot_id = PinSlot(&app_admin_request_array);
+			if (interaction_q_slot_id == -1)
 			{
 				LOG_ERROR_MESSAGE("PinSlot failed");
 				return W_ERROR;
 			}
-			data = &(app_admin_request_array.odbc_data_array[interaction_q_slot_id].admin_request_odbc_data.eb);
+			data = &(app_admin_request_array.data_array[interaction_q_slot_id].admin_request_data);
 			length = sizeof(struct admin_request_t);
 			break;
 		case BEST_SELLERS:
-			interaction_q_slot_id=PinSlot(&app_best_sellers_array);
-			if (interaction_q_slot_id==-1)
+			interaction_q_slot_id = PinSlot(&app_best_sellers_array);
+			if (interaction_q_slot_id == -1)
 			{
 				LOG_ERROR_MESSAGE("PinSlot failed");
 				return W_ERROR;
 			}
-			data = &(app_best_sellers_array.odbc_data_array[interaction_q_slot_id].best_sellers_odbc_data.eb);
+			data = &(app_best_sellers_array.data_array[interaction_q_slot_id].best_sellers_data);
 			length = sizeof(struct best_sellers_t);
 			break;
 		case BUY_CONFIRM:
-			interaction_q_slot_id=PinSlot(&app_buy_confirm_array);
-			if (interaction_q_slot_id==-1)
+			interaction_q_slot_id = PinSlot(&app_buy_confirm_array);
+			if (interaction_q_slot_id == -1)
 			{
 				LOG_ERROR_MESSAGE("PinSlot failed");
 				return W_ERROR;
 			}
-			data = &(app_buy_confirm_array.odbc_data_array[interaction_q_slot_id].buy_confirm_odbc_data.eb);
+			data = &(app_buy_confirm_array.data_array[interaction_q_slot_id].buy_confirm_data);
 			length = sizeof(struct buy_confirm_t);
 			break;
 		case BUY_REQUEST:
-			interaction_q_slot_id=PinSlot(&app_buy_request_array);
-			if (interaction_q_slot_id==-1)
+			interaction_q_slot_id = PinSlot(&app_buy_request_array);
+			if (interaction_q_slot_id == -1)
 			{
 				LOG_ERROR_MESSAGE("PinSlot failed");
 				return W_ERROR;
 			}
-			data = &(app_buy_request_array.odbc_data_array[interaction_q_slot_id].buy_request_odbc_data.eb);
+			data = &(app_buy_request_array.data_array[interaction_q_slot_id].buy_request_data);
 			length = sizeof(struct buy_request_t);
 			break;
 		case HOME:
-			interaction_q_slot_id=PinSlot(&app_home_array);
-			if (interaction_q_slot_id==-1)
+			interaction_q_slot_id = PinSlot(&app_home_array);
+			if (interaction_q_slot_id == -1)
 			{
 				LOG_ERROR_MESSAGE("PinSlot failed");
 				return W_ERROR;
 			}
-			data = &(app_home_array.odbc_data_array[interaction_q_slot_id].home_odbc_data.eb);
+			data = &(app_home_array.data_array[interaction_q_slot_id].home_data);
 			length = sizeof(struct home_t);
 			break; 
 		case NEW_PRODUCTS:
-			interaction_q_slot_id=PinSlot(&app_new_products_array);
-			if (interaction_q_slot_id==-1)
+			interaction_q_slot_id = PinSlot(&app_new_products_array);
+			if (interaction_q_slot_id == -1)
 			{
 				LOG_ERROR_MESSAGE("PinSlot failed");
 				return W_ERROR;
 			}
-			data = &(app_new_products_array.odbc_data_array[interaction_q_slot_id].new_products_odbc_data.eb);
+			data = &(app_new_products_array.data_array[interaction_q_slot_id].new_products_data);
 			length = sizeof(struct new_products_t);
 			break;
 		case ORDER_DISPLAY:
-			interaction_q_slot_id=PinSlot(&app_order_display_array);
-			if (interaction_q_slot_id==-1)
+			interaction_q_slot_id = PinSlot(&app_order_display_array);
+			if (interaction_q_slot_id == -1)
 			{
 				LOG_ERROR_MESSAGE("PinSlot failed");
 				return W_ERROR;
 			}
-			data = &(app_order_display_array.odbc_data_array[interaction_q_slot_id].order_display_odbc_data.eb);
+			data = &(app_order_display_array.data_array[interaction_q_slot_id].order_display_data);
 			length = sizeof(struct order_display_t);
 			break;
 		case ORDER_INQUIRY:
-			interaction_q_slot_id=PinSlot(&app_order_inquiry_array);
-			if (interaction_q_slot_id==-1)
+			interaction_q_slot_id = PinSlot(&app_order_inquiry_array);
+			if (interaction_q_slot_id == -1)
 			{
 				LOG_ERROR_MESSAGE("PinSlot failed");
 				return W_ERROR;
 			}
-			data = &(app_order_inquiry_array.odbc_data_array[interaction_q_slot_id].order_inquiry_odbc_data.eb);
+			data = &(app_order_inquiry_array.data_array[interaction_q_slot_id].order_inquiry_data);
 			length = sizeof(struct order_inquiry_t);
 			break;
 		case PRODUCT_DETAIL:
-			interaction_q_slot_id=PinSlot(&app_product_detail_array);
-			if (interaction_q_slot_id==-1)
+			interaction_q_slot_id = PinSlot(&app_product_detail_array);
+			if (interaction_q_slot_id == -1)
 			{
 				LOG_ERROR_MESSAGE("PinSlot failed");
 				return W_ERROR;
 			}
-			data = &(app_product_detail_array.odbc_data_array[interaction_q_slot_id].product_detail_odbc_data.eb);
+			data = &(app_product_detail_array.data_array[interaction_q_slot_id].product_detail_data);
 			length = sizeof(struct product_detail_t);
 			break;
 		case SEARCH_REQUEST:
-			interaction_q_slot_id=PinSlot(&app_search_request_array);
-			if (interaction_q_slot_id==-1)
+			interaction_q_slot_id = PinSlot(&app_search_request_array);
+			if (interaction_q_slot_id == -1)
 			{
 				LOG_ERROR_MESSAGE("PinSlot failed");
 				return W_ERROR;
 			}
-			data = &(app_search_request_array.odbc_data_array[interaction_q_slot_id].search_request_odbc_data.eb);
+			data = &(app_search_request_array.data_array[interaction_q_slot_id].search_request_data);
 			length = sizeof(struct search_request_t);
 			break;
 		case SEARCH_RESULTS:
-			interaction_q_slot_id=PinSlot(&app_search_results_array);
-			if (interaction_q_slot_id==-1)
+			interaction_q_slot_id = PinSlot(&app_search_results_array);
+			if (interaction_q_slot_id == -1)
 			{
 				LOG_ERROR_MESSAGE("PinSlot failed");
 				return W_ERROR;
 			}
-			data = &(app_search_results_array.odbc_data_array[interaction_q_slot_id].search_results_odbc_data.eb);
+			data = &(app_search_results_array.data_array[interaction_q_slot_id].search_results_data);
 			length = sizeof(struct search_results_t);
 			break;
 		case SHOPPING_CART:
-			interaction_q_slot_id=PinSlot(&app_shopping_cart_array);
-			if (interaction_q_slot_id==-1)
+			interaction_q_slot_id = PinSlot(&app_shopping_cart_array);
+			if (interaction_q_slot_id == -1)
 			{
 				LOG_ERROR_MESSAGE("PinSlot failed");
 				return W_ERROR;
 			}
-			data = &(app_shopping_cart_array.odbc_data_array[interaction_q_slot_id].shopping_cart_odbc_data.eb);
+			data = &(app_shopping_cart_array.data_array[interaction_q_slot_id].shopping_cart_data);
 			length = sizeof(struct shopping_cart_t);
 			break;
 	}
-	TxnQItem->SlotID=interaction_q_slot_id;
+	TxnQItem->SlotID = interaction_q_slot_id;
 	if (_receive(s, data, length) == -1)
 	{
 		LOG_ERROR_MESSAGE("cannot receive interaction data");
@@ -198,7 +199,7 @@ int send_transaction_packet(int s, struct QItem TxnQItem)
 	int interaction;
 	int result;
 
-	interaction=TxnQItem.TxnType;
+	interaction = TxnQItem.TxnType;
 	if (_send(s, &interaction, sizeof(int)) == -1)
 	{
 		LOG_ERROR_MESSAGE("cannot send interaction type");
@@ -208,7 +209,7 @@ int send_transaction_packet(int s, struct QItem TxnQItem)
 	{
 		case ADMIN_CONFIRM:
 			result = app_admin_confirm_array.txn_result[TxnQItem.SlotID];
-				break;
+			break;
 		case ADMIN_REQUEST:
 			result = app_admin_request_array.txn_result[TxnQItem.SlotID];
 			break;
@@ -252,64 +253,60 @@ int send_transaction_packet(int s, struct QItem TxnQItem)
 		return W_ERROR;
 	}
 	
-	if (result==OK)
+	if (result == OK)
 	{
 		switch (interaction)
 		{
 			case ADMIN_CONFIRM:
-				data = &(app_admin_confirm_array.odbc_data_array[TxnQItem.SlotID].admin_confirm_odbc_data.eb);
+				data = &(app_admin_confirm_array.data_array[TxnQItem.SlotID].admin_confirm_data);
 				length = sizeof(struct admin_confirm_t);
 				break;
 			case ADMIN_REQUEST:
-				data = &(app_admin_request_array.odbc_data_array[TxnQItem.SlotID].admin_request_odbc_data.eb);
+				data = &(app_admin_request_array.data_array[TxnQItem.SlotID].admin_request_data);
 				length = sizeof(struct admin_request_t);
 				break;
 			case BEST_SELLERS:
-				data = &(app_best_sellers_array.odbc_data_array[TxnQItem.SlotID].best_sellers_odbc_data.eb);
+				data = &(app_best_sellers_array.data_array[TxnQItem.SlotID].best_sellers_data);
 				length = sizeof(struct best_sellers_t);
 				break;
 			case BUY_CONFIRM:
-				data = &(app_buy_confirm_array.odbc_data_array[TxnQItem.SlotID].buy_confirm_odbc_data.eb);
+				data = &(app_buy_confirm_array.data_array[TxnQItem.SlotID].buy_confirm_data);
 				length = sizeof(struct buy_confirm_t);
 				break;
 			case BUY_REQUEST:
-				data = &(app_buy_request_array.odbc_data_array[TxnQItem.SlotID].buy_request_odbc_data.eb);
+				data = &(app_buy_request_array.data_array[TxnQItem.SlotID].buy_request_data);
 				length = sizeof(struct buy_request_t);
 				break;
 			case HOME:
-	#ifdef DEBUG
-				DEBUGMSG("thread_id%d: send pkg txntype %d, slot %d", pthread_self(), TxnQItem.TxnType, TxnQItem.SlotID);
-				DEBUGMSG("thread_id%d: c_fname:%s, c_lname:%s", pthread_self(), app_home_array.odbc_data_array[TxnQItem.SlotID].home_odbc_data.eb.c_fname, app_home_array.odbc_data_array[TxnQItem.SlotID].home_odbc_data.eb.c_lname);
-	#endif
-				data = &(app_home_array.odbc_data_array[TxnQItem.SlotID].home_odbc_data.eb);
+				data = &(app_home_array.data_array[TxnQItem.SlotID].home_data);
 				length = sizeof(struct home_t);
 				break; 
 			case NEW_PRODUCTS:
-				data = &(app_new_products_array.odbc_data_array[TxnQItem.SlotID].new_products_odbc_data.eb);
+				data = &(app_new_products_array.data_array[TxnQItem.SlotID].new_products_data);
 				length = sizeof(struct new_products_t);
 				break;
 			case ORDER_DISPLAY:
-				data = &(app_order_display_array.odbc_data_array[TxnQItem.SlotID].order_display_odbc_data.eb);
+				data = &(app_order_display_array.data_array[TxnQItem.SlotID].order_display_data);
 				length = sizeof(struct order_display_t);
 				break;
 			case ORDER_INQUIRY:
-				data = &(app_order_inquiry_array.odbc_data_array[TxnQItem.SlotID].order_inquiry_odbc_data.eb);
+				data = &(app_order_inquiry_array.data_array[TxnQItem.SlotID].order_inquiry_data);
 				length = sizeof(struct order_inquiry_t);
 				break;
 			case PRODUCT_DETAIL:
-				data = &(app_product_detail_array.odbc_data_array[TxnQItem.SlotID].product_detail_odbc_data.eb);
+				data = &(app_product_detail_array.data_array[TxnQItem.SlotID].product_detail_data);
 				length = sizeof(struct product_detail_t);
 				break;
 			case SEARCH_REQUEST:
-				data = &(app_search_request_array.odbc_data_array[TxnQItem.SlotID].search_request_odbc_data.eb);
+				data = &(app_search_request_array.data_array[TxnQItem.SlotID].search_request_data);
 				length = sizeof(struct search_request_t);
 				break;
 			case SEARCH_RESULTS:
-				data = &(app_search_results_array.odbc_data_array[TxnQItem.SlotID].search_results_odbc_data.eb);
+				data = &(app_search_results_array.data_array[TxnQItem.SlotID].search_results_data);
 				length = sizeof(struct search_results_t);
 				break;
 			case SHOPPING_CART:
-				data = &(app_shopping_cart_array.odbc_data_array[TxnQItem.SlotID].shopping_cart_odbc_data.eb);
+				data = &(app_shopping_cart_array.data_array[TxnQItem.SlotID].shopping_cart_data);
 				length = sizeof(struct shopping_cart_t);
 				break;
 		}
