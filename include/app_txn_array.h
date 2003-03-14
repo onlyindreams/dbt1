bdf4e7b5b72abde432ddcd6696090bc8ee00b768
jztpcw <jztpcw@eccdf8fb-1411-0410-9b6e-c06b97aefaee>
2003-03-14 20:30:59 +0000

    moved transaction array definition from appServer.c to app_txn_array.h
    git-svn-id: https://osdldbt.svn.sourceforge.net/svnroot/osdldbt/trunk/dbt1@830 eccdf8fb-1411-0410-9b6e-c06b97aefaee

--- include/app_txn_array.h
+++ include/app_txn_array.h
@@ -1,4 +1,5 @@
-/* app_txn_array.h 
+/*
+ * app_txn_array.h 
  * array of odbc_data_t to hold input/output for each transaction
  *
  * This file is released under the terms of the Artistic License.  Please see
@@ -8,25 +9,42 @@
  *                    Open Source Development Lab, Inc.
  *
  */
-#ifndef _APP_TXN_ARRAY_H
-#define _APP_TXN_ARRAY_H
-#include <pthread.h>
-#include "odbc_interaction.h"
 
+#ifndef _APP_TXN_ARRAY_H_
+#define _APP_TXN_ARRAY_H_
+
+#include <pthread.h>
+#include <interaction_data.h>
 
 struct app_txn_array
 {
 	int size;
 	int *pin;
-	pthread_mutex_t odbc_txn_array_mutex;
-	union odbc_data_t *odbc_data_array;
+	pthread_mutex_t txn_array_mutex;
+	union interaction_data_t *data_array;
 	int *txn_result;
 #ifdef GET_TIME
 	double *db_response_time;
 #endif
 };
 
+/* the txn input output array */
+extern struct app_txn_array app_admin_confirm_array;
+extern struct app_txn_array app_admin_request_array;
+extern struct app_txn_array app_best_sellers_array;
+extern struct app_txn_array app_buy_confirm_array;
+extern struct app_txn_array app_buy_request_array;
+extern struct app_txn_array app_home_array;
+extern struct app_txn_array app_new_products_array;
+extern struct app_txn_array app_order_display_array;
+extern struct app_txn_array app_order_inquiry_array;
+extern struct app_txn_array app_product_detail_array;
+extern struct app_txn_array app_search_request_array;
+extern struct app_txn_array app_search_results_array;
+extern struct app_txn_array app_shopping_cart_array;
+
 void init_app_txn_array(struct app_txn_array *txn_array, int ArraySize);
 int PinSlot(struct app_txn_array *txn_array);
 int FreeSlot(struct app_txn_array *txn_array, int SlotID);
-#endif
+
+#endif /* _APP_TXN_ARRAY_H_ */
