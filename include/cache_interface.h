119fea0a0a491d05a77a9f33726eba9eadf00eff
jztpcw <jztpcw@eccdf8fb-1411-0410-9b6e-c06b97aefaee>
2003-03-14 20:35:07 +0000

    include interaction_data.h instead of odbc_interaction.h
    git-svn-id: https://osdldbt.svn.sourceforge.net/svnroot/osdldbt/trunk/dbt1@831 eccdf8fb-1411-0410-9b6e-c06b97aefaee

--- include/cache_interface.h
+++ include/cache_interface.h
@@ -1,5 +1,5 @@
-/* app_interface.h 
- * appServer interface for sending/receiving packet to/from eb
+/*
+ * cache_interface.h 
  *
  * This file is released under the terms of the Artistic License.  Please see
  * the file LICENSE, included in this package, for details.
@@ -8,13 +8,19 @@
  *                    Open Source Development Lab, Inc.
  *
  */
+
 #ifndef _CACHE_INTERFACE_H
 #define _CACHE_INTERFACE_H_
-#include <errno.h>
-#include <common.h>
-#include <_socket.h>
-#include <odbc_interaction.h>
 
-int receive_search_results(int s, struct search_results_t *search_results_request);
-int send_search_results(int s,  struct search_results_t *search_results_response);
-#endif
+#include <errno.h>
+
+#include <common.h>
+#include <interaction_data.h>
+#include <_socket.h>
+
+int receive_search_results(int s,
+	struct search_results_t *search_results_request);
+int send_search_results(int s, 
+	struct search_results_t *search_results_response);
+
+#endif /* _CACHE_INTERFACE_H_ */
