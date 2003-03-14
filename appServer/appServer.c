98fb65b1b4abe48687d9eee71097d87f9916a752
jztpcw <jztpcw@eccdf8fb-1411-0410-9b6e-c06b97aefaee>
2003-03-14 20:03:24 +0000

    Using  getopt to parse command line parameters, Added the ability for the appServer to specify the output directory for their output files; Applied a consistent style in various places; Start the listener in the appServer before initializing database connections; Introduced an 'altered' variable for the threading experiment, which is intended to only prevent the applications from quitting just because it can't create any more threads
    git-svn-id: https://osdldbt.svn.sourceforge.net/svnroot/osdldbt/trunk/dbt1@820 eccdf8fb-1411-0410-9b6e-c06b97aefaee

--- appServer/appServer.c
+++ appServer/appServer.c
@@ -8,15 +8,16 @@
  *
  */
 
-#include <string.h>
+#include <pthread.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/time.h>
 #include <sys/socket.h>
 #include <sys/resource.h>
 #include <sys/wait.h>
 #include <errno.h>
-#include <pthread.h>
+#include <ctype.h>
 #include <semaphore.h>
 #include <unistd.h>
 #include <signal.h>
@@ -32,53 +33,35 @@ void sighandler(int signum);
 
 static int connectioncount = 0;
 
-/*globals*/
-/*array to hold odbc input/output data
-  though the data structure is the same for all the transaction,
-  create an array for each transaction so that there is less contention
-  trying to pin the slot
-*/
-struct app_txn_array app_admin_confirm_array;
-struct app_txn_array app_admin_request_array;
-struct app_txn_array app_best_sellers_array;
-struct app_txn_array app_buy_confirm_array;
-struct app_txn_array app_buy_request_array;
-struct app_txn_array app_home_array;
-struct app_txn_array app_new_products_array;
-struct app_txn_array app_order_display_array;
-struct app_txn_array app_order_inquiry_array;
-struct app_txn_array app_product_detail_array;
-struct app_txn_array app_search_request_array;
-struct app_txn_array app_search_results_array;
-struct app_txn_array app_shopping_cart_array;
+/* globals */
 
-/*mutex to control access to the queue*/
+/* mutex to control access to the queue */
 pthread_mutex_t queue_mutex;
 
-/*semaphore to wait in DoTxn thread, each time a queue item is
-  added to the queue, the semaphore must be released once
-*/
+/* semaphore to wait in DoTxn thread, each time a queue item is
+ * added to the queue, the semaphore must be released once
+ */
 sem_t TxnQSem;
 
-/*the transaction queue*/
+/* the transaction queue */
 struct Queue TxnQ;
 
-/*mutex for each each entry in the queue, this is used to 
-  notify the DoConnection thread that the transaction is done
-*/
+/* mutex for each each entry in the queue, this is used to 
+ * notify the DoConnection thread that the transaction is done
+ */
 struct condition_bundle_t *queue_entry_condition;
 
 #ifdef GET_TIME
 FILE *timefp;
-pthread_mutex_t time_log_mutex=PTHREAD_MUTEX_INITIALIZER;
-#endif
+pthread_mutex_t time_log_mutex = PTHREAD_MUTEX_INITIALIZER;
+#endif /* GET_TIME */
 
-#ifdef SEARCH_RESULTS_CACHE
 char search_results_cache_host[32];
 int search_results_cache_port;
-#endif
 
-pthread_mutex_t mutex_app_server=PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex_app_server = PTHREAD_MUTEX_INITIALIZER;
+
+int altered = 0;
 
 int main(int argc, char *argv[]) 
 {
@@ -90,6 +73,10 @@ int main(int argc, char *argv[])
 	int rec;
 	int port, PoolThreads, TxnQSize, ArraySize;
 	struct sigaction sa;
+	int c;
+#ifdef GET_TIME
+	char filename[512];
+#endif /* GET_TIME */
 
 	setlinebuf(stdout);
 	memset(&sa, 0, sizeof(sa));
@@ -107,32 +94,94 @@ int main(int argc, char *argv[])
 
 	if (argc < 9)
 	{
-#ifdef SEARCH_RESULTS_CACHE
-		printf("usage: appServer <dbnodename> <username> <password> <port> <db_connection> <transaction queue size> <transaction array size> <items> <search_results_cache_host> <search_results_cache_port>\n");
-#else
-		printf("usage: appServer <dbnodename> <username> <password> <port> <db_connection> <transaction queue size> <transaction array size> <items>\n");
-#endif
+		printf("usage: appServer -d <dbnodename> -u <username> -p <password> -l <port>\n");
+		printf("                 -c <db_connection> -q <transaction queue size>\n");
+		printf("                 -a <transaction array size> -i <items>\n");
+		printf("                 -x <search_results_cache_host> -y <search_results_cache_port>\n");
+		printf("                 -m <0|1>\n");
+		printf("\n");
+		printf("-m 1 accesses the cache and requires -x and -y, default\n");
+		printf("-m 0 does not access the cache\n");
 		return -1;
 	}
-	
-	strcpy(sname, argv[1]);
-	strcpy(uname, argv[2]);
-	strcpy(auth, argv[3]);
-	port = atoi(argv[4]);
-	PoolThreads=atoi(argv[5]);
-	TxnQSize=atoi(argv[6]);
-	ArraySize=atoi(argv[7]);
-	item_count=atoi(argv[8]);
-#ifdef SEARCH_RESULTS_CACHE
-	strcpy(search_results_cache_host, argv[9]);
-	search_results_cache_port=atoi(argv[10]);
-#endif
+
+	opterr = 0;
+	while ((c = getopt(argc, argv, "a:c:d:i:l:m:o:p:q:u:x:y:z")) != -1)
+	{
+		switch (c)
+		{
+			case 'a':
+				ArraySize = atoi(optarg);
+				break;
+			case 'c':
+				PoolThreads = atoi(optarg);
+				break;
+			case 'd':
+				strcpy(sname, optarg);
+				break;
+			case 'i':
+				item_count = atoi(optarg);
+				break;
+			case 'l':
+				port = atoi(optarg);
+				break;
+			case 'm':
+				mode_cache = atoi(optarg);
+				break;
+			case 'o':
+				strcpy(output_path, optarg);
+				break;
+			case 'p':
+				strcpy(auth, optarg);
+				break;
+			case 'q':
+				TxnQSize = atoi(optarg);
+				break;
+			case 'u':
+				strcpy(uname, optarg);
+				break;
+			case 'x':
+				strcpy(search_results_cache_host, optarg);
+				break;
+			case 'y':
+				search_results_cache_port = atoi(optarg);
+				break;
+			case 'z':
+				altered = 1;
+				break;
+			case '?':
+				if (isprint(optopt))
+				{
+					fprintf(stderr, "Unknown option `-%c'.\n", optopt);
+				}
+				else
+				{
+					fprintf (stderr, "Unknown option character `\\x%x'.\n",
+						optopt);  
+				}
+				return 1;
+			default:
+				return -1;
+		}
+	}
+
 	init_common();
+
+	mastersock = _server_init_socket(port);
+
+	if (mastersock < 0)
+	{
+		LOG_ERROR_MESSAGE("init server master socket failed\n");
+		printf("init server master socket failed\n");
+		return -1;
+	}
+
 	/* create the threadpool. */
 	if (!init_thread_pool(PoolThreads, TxnQSize, sname, uname, auth))
 	{
-		LOG_ERROR_MESSAGE("InitThreadPool() failed with %d connection", PoolThreads);
-      		return -1;
+		LOG_ERROR_MESSAGE("InitThreadPool() failed with %d connection",
+			PoolThreads);
+		return -1;
   	}
 
 	/* Initialize app_txn_array */
@@ -149,65 +198,83 @@ int main(int argc, char *argv[])
 	init_app_txn_array(&app_search_request_array, ArraySize);
 	init_app_txn_array(&app_search_results_array, ArraySize);
 	init_app_txn_array(&app_shopping_cart_array, ArraySize);
-        /* initialize TxnQ */
+	/* initialize TxnQ */
 	init_queue(&TxnQ, TxnQSize);
 #ifdef GET_TIME
-	timefp = fopen("time.log", "w");
-        if (timefp == NULL)
-        {
-                LOG_ERROR_MESSAGE("cannot open file time.log\n");
-                return -1;
-        }
-#endif
-	mastersock = _server_init_socket(port);
-
-	if (mastersock < 0)
+	if (output_path[0] != '\0')
 	{
-		LOG_ERROR_MESSAGE("init server master socket failed\n");
-		printf("init server master socket failed");
+		sprintf(filename, "%stime.log", output_path);
+	}
+	else
+	{
+		strcpy(filename, "time.log");
+	}
+	timefp = fopen(filename, "w");
+	if (timefp == NULL)
+	{
+		LOG_ERROR_MESSAGE("cannot open file time.log\n");
 		return -1;
 	}
+#endif /* GET_TIME */
 
-	printf("The server is active...\n");
-
+	printf("The app server is active...\n");
 	while (1) {
 		addrlen = sizeof(socketaddr);
 		pthread_mutex_lock(&mutex_app_server);
-		workersock = accept(mastersock, (struct sockaddr *)&socketaddr, (socklen_t *)&addrlen);
+		workersock = accept(mastersock, (struct sockaddr *) &socketaddr,
+			(socklen_t *)&addrlen);
 		if (workersock < 0) {
-			LOG_ERROR_MESSAGE("accept couldn't open worker socket, errno %d", errno);
-			printf("accept failed");
-			return -1;
+			printf("accept failed: %d\n", errno);
+			if (altered == 0)
+			{
+				LOG_ERROR_MESSAGE(
+					"accept couldn't open worker socket, errno %d", errno);
+				return -1;
+			}
+			break;
 		}
 
 		connectioncount++;
-		printf("%d users logged in\n", connectioncount);
-		if ((rec=pthread_create(&ConnThread, NULL, DoConnection, &workersock)) != 0)    
+		if ((rec = pthread_create(&ConnThread, NULL, DoConnection,
+			&workersock)) != 0)
 		{
-			LOG_ERROR_MESSAGE("pthread_create failed, rec=%d", rec);
-			return -1;
+			if (altered == 0)
+			{
+				LOG_ERROR_MESSAGE("pthread_create failed, rec=%d", rec);
+				return -1;
+			}
+			--connectioncount;
+			printf("Connection attempt made, but we cannot start a new pthread.  Stop listening...");
+			break;
 		}
+		printf("%d client connections have been established.\n", connectioncount);
+	}
+	close(mastersock);
+	printf("%d client connections have been established.\n", connectioncount);
+	while (1)
+	{
+		sleep(60);
 	}
 	printf("Shutting down.\n");
 
-	close(mastersock);
  
 	return 0;
 }
 
 void sighandler(int signum)
 {
-        if (signum==SIGUSR1)
+	if (signum == SIGUSR1)
 		printf("db connect thread failed, please check error.log\n");
-        else if (signum==SIGINT)
+	else if (signum == SIGINT)
 	{
-                printf("program ended by user\n");
-		popen("ps -ef|grep appServer|grep -v 'grep' |awk '{print $2}'| xargs -t -i kill -9 {}", "r");
+		printf("program ended by user\n");
+		popen("ps -ef | grep appServer|grep -v 'grep' | awk '{print $2}' | xargs -t -i kill -9 {}", "r");
 		exit(1);
 	}
-        _exit(-1);
+	_exit(-1);
 }
 
+/* Handle connection requests from a user. */
 void *DoConnection(void *fd)
 {
 	int *sf;
@@ -218,41 +285,36 @@ void *DoConnection(void *fd)
 #ifdef GET_TIME
 	struct timeval receive_request_time,  send_response_time, t1;
 	double server_response_time, db_response_time = 0, server_db_time;
-#endif
+#endif /* GET_TIME */
 	
 	sf = (int *)(fd);
 	workersock = *sf;
 	pthread_mutex_unlock(&mutex_app_server);
-#ifdef DEBUG
-	DEBUGMSG("pthread_id%ld: DoConnection: got connection", pthread_self());
-#endif
 	while (1)
 	{
-		if((rec=receive_transaction_packet(workersock, &TxnQItem)) == W_ERROR) 
+		if ((rec = receive_transaction_packet(workersock, &TxnQItem)) ==
+			W_ERROR) 
 		{
 			LOG_ERROR_MESSAGE("receive_transaction_packet failed");
 			close(workersock);
 			pthread_exit(NULL);
 		}
-		if (rec==SOCKET_CLOSE)
+		if (rec == SOCKET_CLOSE)
 		{
 			LOG_ERROR_MESSAGE("driver socket closed");
 			close(workersock);
 			pthread_exit(NULL);
 		}
-#ifdef DEBUG
-		DEBUGMSG("pthread_id%ld: TxnQItem txn_id %d, slot %d", pthread_self(), TxnQItem.TxnType, TxnQItem.SlotID);
-#endif
 #ifdef GET_TIME
 		if (gettimeofday(&receive_request_time, NULL)== -1)
 		{
 			LOG_ERROR_MESSAGE("gettimeofday failed");
 		}
-#endif
-		/*add interaction to txnQ */
+#endif /* GET_TIME */
+		/* add interaction to txnQ */
 		pthread_mutex_lock(&queue_mutex);
-		QIndex=enqueue(TxnQItem, &TxnQ);
-		if (QIndex==-1)
+		QIndex = enqueue(TxnQItem, &TxnQ);
+		if (QIndex == -1)
 		{
 			pthread_mutex_unlock(&queue_mutex);
 			close(workersock);
@@ -263,71 +325,80 @@ void *DoConnection(void *fd)
 
 		sem_post(&TxnQSem);
 #ifdef GET_TIME
-		if (gettimeofday(&t1, NULL)== -1)
+		if (gettimeofday(&t1, NULL) == -1)
 		{
 			LOG_ERROR_MESSAGE("gettimeofday failed");
 		}
-#endif
+#endif /* GET_TIME */
 		/* wait for transation done */
-#ifdef DEBUG
-		DEBUGMSG("pthread_id%ld: wait for txn done...", pthread_self());
-#endif
 		pthread_mutex_lock(&queue_entry_condition[QIndex].condition_mutex);
 		while (!queue_entry_condition[QIndex].done_flag)
-			pthread_cond_wait(&queue_entry_condition[QIndex].done_cv, &queue_entry_condition[QIndex].condition_mutex);
-		queue_entry_condition[QIndex].done_flag=0;
+			pthread_cond_wait(&queue_entry_condition[QIndex].done_cv,
+				&queue_entry_condition[QIndex].condition_mutex);
+		queue_entry_condition[QIndex].done_flag = 0;
 		pthread_mutex_unlock(&queue_entry_condition[QIndex].condition_mutex);
 			
-#ifdef DEBUG
-		DEBUGMSG("pthread_id%ld: got it, txn done", pthread_self());
-#endif
 #ifdef GET_TIME
-		if (gettimeofday(&send_response_time, NULL)== -1)
+		if (gettimeofday(&send_response_time, NULL) == -1)
 		{
 			LOG_ERROR_MESSAGE("gettimeofday failed");
 		}
-		server_response_time=time_diff(receive_request_time, send_response_time);
-		server_db_time=time_diff(t1,send_response_time);
+		server_response_time =
+			time_diff(receive_request_time, send_response_time);
+		server_db_time = time_diff(t1,send_response_time);
 		switch (TxnQItem.TxnType)
 		{
 			case ADMIN_CONFIRM:
-				db_response_time = app_admin_confirm_array.db_response_time[TxnQItem.SlotID];
+				db_response_time =
+					app_admin_confirm_array.db_response_time[TxnQItem.SlotID];
 				break;
 			case ADMIN_REQUEST:
-				db_response_time = app_admin_request_array.db_response_time[TxnQItem.SlotID];
+				db_response_time =
+					app_admin_request_array.db_response_time[TxnQItem.SlotID];
 				break;
 			case BEST_SELLERS:
-				db_response_time = app_best_sellers_array.db_response_time[TxnQItem.SlotID];
+				db_response_time =
+					app_best_sellers_array.db_response_time[TxnQItem.SlotID];
 				break;
 			case BUY_CONFIRM:
-				db_response_time = app_buy_confirm_array.db_response_time[TxnQItem.SlotID];
+				db_response_time =
+					app_buy_confirm_array.db_response_time[TxnQItem.SlotID];
 				break;
 			case BUY_REQUEST:
-				db_response_time = app_buy_request_array.db_response_time[TxnQItem.SlotID];
+				db_response_time =
+					app_buy_request_array.db_response_time[TxnQItem.SlotID];
 				break;
 			case HOME:
-				db_response_time = app_home_array.db_response_time[TxnQItem.SlotID];
+				db_response_time =
+					app_home_array.db_response_time[TxnQItem.SlotID];
 				break;
 			case NEW_PRODUCTS:
-				db_response_time = app_new_products_array.db_response_time[TxnQItem.SlotID];
+				db_response_time =
+					app_new_products_array.db_response_time[TxnQItem.SlotID];
 				break;
 			case ORDER_DISPLAY:
-				db_response_time = app_order_display_array.db_response_time[TxnQItem.SlotID];
+				db_response_time =
+					app_order_display_array.db_response_time[TxnQItem.SlotID];
 				break;
 			case ORDER_INQUIRY:
-				db_response_time = app_order_inquiry_array.db_response_time[TxnQItem.SlotID];
+				db_response_time =
+					app_order_inquiry_array.db_response_time[TxnQItem.SlotID];
 				break;
 			case PRODUCT_DETAIL:
-				db_response_time = app_product_detail_array.db_response_time[TxnQItem.SlotID];
+				db_response_time =
+					app_product_detail_array.db_response_time[TxnQItem.SlotID];
 				break;
 			case SEARCH_REQUEST:
-				db_response_time = app_search_request_array.db_response_time[TxnQItem.SlotID];
+				db_response_time =
+					app_search_request_array.db_response_time[TxnQItem.SlotID];
 				break;
 			case SEARCH_RESULTS:
-				db_response_time = app_search_results_array.db_response_time[TxnQItem.SlotID];
+				db_response_time =
+					app_search_results_array.db_response_time[TxnQItem.SlotID];
 				break;
 			case SHOPPING_CART:
-				db_response_time = app_shopping_cart_array.db_response_time[TxnQItem.SlotID];
+				db_response_time =
+					app_shopping_cart_array.db_response_time[TxnQItem.SlotID];
 				break;
 		}
 		pthread_mutex_lock(&time_log_mutex);
@@ -336,7 +407,7 @@ void *DoConnection(void *fd)
 			server_response_time, db_response_time, server_db_time);
 		fflush(timefp);
 		pthread_mutex_unlock(&time_log_mutex);
-#endif
+#endif /* GET_TIME */
 		if(send_transaction_packet(workersock, TxnQItem) != OK) 
 		{
 			LOG_ERROR_MESSAGE("send_transaction_packet failed");
