e9447b783bd6d44f29ed886d87f9bc74c393a19c
jztpcw <jztpcw@eccdf8fb-1411-0410-9b6e-c06b97aefaee>
2003-05-28 23:13:49 +0000

    create database
    git-svn-id: https://osdldbt.svn.sourceforge.net/svnroot/osdldbt/trunk/dbt1@1032 eccdf8fb-1411-0410-9b6e-c06b97aefaee

---
+++ scripts/pgsql/create_db.sh
@@ -0,0 +1,48 @@
+#! /bin/sh
+
+# create_db.sh
+#
+# This file is released under the terms of the Artistic License.  Please see
+# the file LICENSE, included in this package, for details.
+#
+# Copyright (C) 2002 Open Source Development Lab, Inc.
+# Author: Jenny Zhang based on the scripts submitted by
+#	 Virginie Megy & Thierry Missimilly
+#               Bull, Liux Competence Center
+#
+# 28 May 2003
+
+# dont start script as root!
+id=`id | sed s/\(.*// | sed s/uid=//`
+
+if [ "$id" = "0" ]; then
+	echo "dont start script as root"
+	exit 1
+fi
+
+export PATH=/usr/local/pgsql/bin:$PATH
+
+# Postgres env for the database (directory where to put the database files)
+export PGDATA=/dbt1/pgsql
+export PGPORT=5433
+
+# create Postgres DBT1 database
+echo "Creating database..."
+if [ -d $PGDATA ] ; then
+echo "======================================="
+echo "PGData directory $PGDATA already exists"
+echo "Skipping initdb"
+echo "======================================="
+else
+initdb -D $PGDATA
+fi
+
+pg_ctl start -l logfile -D $PGDATA
+
+# Give the database a few seconds to get going
+sleep 5
+createdb $SID1
+
+# create user : dbt is already created by initdb as Linux loging
+#echo "create database user..."
+#createuser -d -A dbt
