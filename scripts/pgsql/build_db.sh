6ab43110c3af63ab0a59b13abf797004261ffc4d
jztpcw <jztpcw@eccdf8fb-1411-0410-9b6e-c06b97aefaee>
2003-06-02 22:19:48 +0000

    tested and working up to create_fk.sh, need to fixed stored procedures before doing anything else
    git-svn-id: https://osdldbt.svn.sourceforge.net/svnroot/osdldbt/trunk/dbt1@1056 eccdf8fb-1411-0410-9b6e-c06b97aefaee

---
+++ scripts/pgsql/build_db.sh
@@ -0,0 +1,83 @@
+#!/bin/sh
+#this script can be used to build the database
+
+if [ $# -ne 0 ] # Script invoked with command-line args
+then
+	getopts "g" Option
+	if [ $Option != "g" ]
+		then echo "usage: $0 -g -d <rdbms> -i <items> -u <eus> -p <path>" 
+		exit
+	else
+		if [ $# -ne 9 ]
+			then echo "usage: $0 -g -d <rdbms> -i <items> -u <eus> -p <path>" 
+			exit
+		else
+			RDBMS=$3
+			ITEMS=$5
+			EUS=$7	
+			DATA_PATH=$9
+		fi
+	fi
+	echo "Generating data... $ITEMS item and $EUS eu"
+	cd ../../datagen
+	date
+	./datagen -d $RDBMS -i $ITEMS -u $EUS -p $DATA_PATH
+	echo "data file is generated"
+	date
+	cd -
+else
+	echo "build the database without generating the data file"
+fi
+	date
+	echo "drop db"
+	./drop_db.sh
+	echo
+	
+	date
+	echo "create db"
+	./create_db.sh
+	echo
+	
+	date
+	echo "create tables"
+	./create_tables.sh
+	echo
+	
+	date
+	echo "load db"
+	./load_db.sh
+	echo
+
+	date
+	echo "create indexes"
+	./create_indexes.sh
+	echo
+	
+	date
+	echo "create foreign key"
+	./create_fk.sh
+	echo
+
+	date
+	echo "create prejoined tables for large database"
+	./add_aux_structure.sh
+	echo
+	
+	date
+	echo "load dbproc"
+	./load_dbproc.sh
+	echo
+	
+	date
+	echo "vacuum"
+        time vacuumdb -z dbt1
+        echo
+
+	echo "start to backup"
+	date
+	
+	echo "backup"
+	./backup_db.sh
+	echo
+	
+	date
