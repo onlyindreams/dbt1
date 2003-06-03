65bf6561c57be91fa45ee6cca90ff63b75a928d5
jztpcw <jztpcw@eccdf8fb-1411-0410-9b6e-c06b97aefaee>
2003-06-03 18:51:36 +0000

    merged Satoshi's code: adding error check
    git-svn-id: https://osdldbt.svn.sourceforge.net/svnroot/osdldbt/trunk/dbt1@1071 eccdf8fb-1411-0410-9b6e-c06b97aefaee

--- scripts/pgsql/create_db.sh
+++ scripts/pgsql/create_db.sh
@@ -6,10 +6,40 @@
 # the file LICENSE, included in this package, for details.
 #
 # Copyright (C) 2002 Open Source Development Lab, Inc.
-# Author: Jenny Zhang
 #
-# 28 May 2003
+# Author NAGAYASU Satoshi, Jenny Zhang
+#
+# Contributer:   Virginie Megy & Thierry Missimilly
+#                Bull, Linux Competence Center
+# June 2003
 
 # pgsql does not create db successfully if createdb is moved to set_db_env.sh
 ./set_db_env.sh
-createdb $SID1
+
+# create database
+echo "create database $SID1..."
+_o=`createdb -U $USER $SID1 2>&1`
+_test=`echo $_o | grep CREATE`
+if [ "$_test" = "" ]; then
+	echo "create $SID1 failed: $_o $_test"
+	exit 1
+fi
+
+echo "create plpgsql on $SID1..."
+_o=`createlang -U $USER -d $SID1 plpgsql 2>&1`
+_test=`echo $_o`
+if [ "$_test" != "" ]; then
+	echo "create plpgsql on $SID1 failed: $_o $_test"
+	exit 1
+fi
+
+# create database demo user
+echo "create database demo user..."
+_o=`psql -c "create user dbt" $SID1 2>&1`
+_test=`echo $_o | grep CREATE`
+if [ "$_test" = "" ]; then
+	echo "create db user failed: $_o"
+	exit 1
+fi
+
+exit 0
